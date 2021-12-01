29d2a7b91a5f3bc034f5589fb1213c790e64ebb3
Lemuria <74448738+a-random-lemurian@users.noreply.github.com>
2021-12-01 23:59:30 +0800

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name="winters",
+    description="Endless Sky utilities",
+    version="0.1.0",
+    author="Lemuria",
+    install_requires=["Click"],
+    entry_points = {
+        'console_scripts': [
+            'winters = winters:entry'
+        ]
+    }
+)
