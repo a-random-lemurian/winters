1bd1ea7d1ff3bfd8933131ce806176169de053a2
Lemuria <74448738+a-random-lemurian@users.noreply.github.com>
2021-12-02 01:23:04 +0800

    add plugin list command

---
+++ winters/pluginlist.py
@@ -0,0 +1,40 @@
+import requests
+import click
+import json
+import re
+from winters.entry import plug as app
+
+
+URL_GH_ESPLUGINS = "https://raw.githubusercontent.com/EndlessSkyCommunity/endless-sky-plugins/master/generated/plugins.json"
+VALID_FORMAT_STYLES = ["compact", "long"]
+HEADERS = {"User-Agent": "winters | Endless Sky utility tool."}
+REGEX_DETECT_GIT_COMMIT_HASH = re.compile('[abcdefABCDEF0123456789]{7,40}')
+
+
+@app.command()
+@click.option('--format-style')
+def list_plugins(format_style = 'compacty'):
+    determine_format_type(format_style)
+
+
+def get_plugins():
+    return requests.get(URL_GH_ESPLUGINS, headers=HEADERS)
+
+
+def determine_format_type(format_style):
+    if format_style not in VALID_FORMAT_STYLES:
+        print('fatal: invalid format type {style}'.format(style=format_style))
+        return(1)
+
+    if format_style == 'compact':
+        print_plugin_compact()
+
+
+def print_plugin_compact():
+    for plugin in json.loads(get_plugins().text):
+        authors = plugin["authors"]
+        if type(authors) == list:
+            authors = f'Multiple ({len(authors)})'
+        print(f"{plugin['name']:<30}{plugin['version']:<45}{authors}")
+
+
