5c906fb4d2cddf83c600226f01ccda81b0f6967f
Lemuria <74448738+a-random-lemurian@users.noreply.github.com>
2021-12-04 11:50:28 +0800

    Explicitly specify modes and encodings

--- winters/pluginmaker.py
+++ winters/pluginmaker.py
@@ -38,5 +38,5 @@ def make_plugin_folder(name):
 
 
 def make_gitignore(plugin_path):
-    with open(os.path.join(plugin_path, '.gitignore')) as gitignore:
+    with open(os.path.join(plugin_path, '.gitignore'), 'r', encoding='utf8') as gitignore:
         gitignore.write('\n'.join(GIT_IGNORE_FILES))
