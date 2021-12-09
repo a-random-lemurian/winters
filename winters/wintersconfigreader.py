d4d68c23692d0594038fec509604832e1e9baea8
Lemuria <74448738+a-random-lemurian@users.noreply.github.com>
2021-12-09 23:13:21 +0800

    Add function to create Wintersconfig file

--- winters/wintersconfigreader.py
+++ winters/wintersconfigreader.py
@@ -6,3 +6,8 @@ import yaml
 def winterscfg(path):
     with open(path, 'r') as winterscfg:
         return yaml.safe_load(winterscfg)
+
+
+def create_wintersconfig(path, data):
+    with open(path, 'x') as winterscfg:
+        yaml.dump(data, winterscfg)
